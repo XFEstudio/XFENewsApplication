9f996fac75835c18382eeaf39e516954bbb572ca
XFEstudio <mail@xfegzs.com>
2025-04-06 12:42:37 +0800

    添加项目文件。

---
+++ XFENewsApplication/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml;
+using Windows.UI.ViewManagement;
+
+namespace XFENewsApplication;
+
+/// <summary>
+/// 主窗体
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public static UISettings UISettings { get; set; } = new UISettings();
+    public MainWindow()
+    {
+        this.InitializeComponent();
+        ExtendsContentIntoTitleBar = true;
+        AppWindow.SetIcon(Path.Combine(AppContext.BaseDirectory, "Assets/Icons/EditorIcon.ico"));
+        AppWindow.TitleBar.PreferredHeightOption = Microsoft.UI.Windowing.TitleBarHeightOption.Tall;
+        UISettings.ColorValuesChanged += (_, _) => DispatcherQueue.TryEnqueue(() => AppThemeHelper.ChangeTheme(SystemProfile.Theme));
+    }
+}
